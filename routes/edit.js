e6576988878bdc16b7c0787f2e5d48a8250a4ea2
Herby Herado <herby,herado@gmail.com>
2018-03-01 21:15:13 +0700

    update latest work

--- routes/edit.js
+++ routes/edit.js
@@ -17,6 +17,29 @@ edit.get('/restaurant', (req, res) => {
             res.render('restaurant-list.ejs', {data: data})
         })
 })
+edit.get('/restaurant/update/:id', (req, res) => {
+    model.Restaurant.findAll()
+        .then(data => {
+            // res.send(data)
+            res.render('restaurant-list.ejs', {data: data})
+        })
+})
+
+edit.post('/restaurant/update/:id', (req, res) => {
+    model.Restaurant.update({
+        Name: req.body.Name,
+        Address: req.body.Address,
+        City: req.body.City
+    })
+})
+
+edit.get('/restaurant/delete/:id', (req, res) => {
+    model.Restaurant.destroy({where: {id: req.params.id}})
+        .then(data => {
+            console.log(data)
+            res.redirect('/restaurant')
+        })
+})
 
 edit.get('/dish', (req, res) => {
     res.render('dish-list.ejs')
