616b72b0705973e71af97e534c55320f5427f730
Herby Herado <herby,herado@gmail.com>
2018-03-01 18:20:03 +0700

    login finished

--- app.js
+++ app.js
@@ -2,11 +2,59 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const dashboard = require('./routes/dashboard')
+const bodyParser = require('body-parser')
+const model = require('./models')
+const session = require('express-session')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+// function checklogin()
+// app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+}))
 
 // Testing
 app.get('/', (req, res) => {
-  res.render('home.ejs')
-  res.status(200).json({ message: 'Connected!'})
+  console.log(req.query)
+  if (!req.query){
+    res.render('home.ejs', {err: null})
+  } else {
+    res.render('home.ejs', {err: req.query})
+  }
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  model.User.findOne({
+    where: {Username: req.body.Username}
+  })
+  .then(user => {
+    if (!user){
+      res.redirect(`/?err=Username+not+found`)
+    } else {
+      model.User.findOne({
+        where: {
+          Username: req.body.Username,
+          Password: req.body.Password
+        }
+      })
+      .then(password => {
+        if (!password){
+          res.redirect('/?err=Incorrect+password')
+        } else {
+          req.session.isLogin = true
+          req.session.profile = user
+          res.redirect(`/dashboard`)
+        }
+      })
+    }
+  })
 })
 
 app.use(express.static('public'))
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,7 +7,8 @@ dashboard.use(bodyParser.urlencoded({extended: false}))
 dashboard.use(bodyParser.json())
 
 // Displays random image on dashboard
-dashboard.get('/:id', (req, res) => {
+dashboard.get('/', (req, res) => {
+    console.log(req.session.profile.id)
     model.Dish.findAll({
         include: [{model: model.UserDish}]
         })
@@ -31,43 +32,43 @@ dashboard.get('/:id', (req, res) => {
         })
 })
 
-dashboard.post('/:id', (req, res) => {
+dashboard.post('/', (req, res) => {
     if (Object.keys(req.body)[0] === 'Reject'){
-        console.log(req.params.id)
+        // console.log(req.params.id)
         // res.send(req.body)
         model.UserDish.create({
-            UserId: req.params.id,
+            UserId: req.session.profile.id,
             DishId: req.body.Reject,
             Like: false,
             Displayed: true
         })
         .then(data => {
             console.log(data)
-            res.redirect(`/dashboard/${req.params.id}`)
+            res.redirect(`/dashboard`)
         })
     } else {
-        console.log(req.params.id)
+        // console.log(req.params.id)
         // res.send(req.body)
         model.UserDish.create({
-            UserId: req.params.id,
+            UserId: req.session.profile.id,
             DishId: req.body.Accept,
             Like: true,
             Displayed: true
         })
         .then(data => {
             console.log(data)
-            res.redirect(`/dashboard/${req.params.id}`)
+            res.redirect(`/dashboard`)
         })
     }
 })
 
-dashboard.get('/:id/reset', (req, res) => {
+dashboard.get('/reset', (req, res) => {
     // res.send('hello')
     model.UserDish.destroy({
-        where: {UserId: req.params.id}
+        where: {UserId: req.session.profile.id}
     })
     .then(() => {
-        res.redirect(`/dashboard/${req.params.id}`)
+        res.redirect(`/dashboard`)
     })
 })
 
